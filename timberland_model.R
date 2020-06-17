ddec8db12ca83eaaf96f6b269940b24cd453ff8d
Yijia Yu <yijia.yu@aimco.ca>
2020-06-17 10:16:47 -0600

    # amend and update the project report

--- timberland_model.R
+++ timberland_model.R
@@ -24,13 +24,14 @@ apply(allts,2,jarque.bera.test)
 apply(allts,2,adf.test)
 apply(allts,2,pp.test)
 
+op = par(mfrow=c(1,2))
 
 #test stationarity and normality
 for(i in 1:ncol(allxts)){
   #plot acf
   acf(na.omit(allxts[,i]),lag.max = 100,xlab = colnames(allxts)[i], ylab = 'ACF', main=' ')
   #plot pacf
-  pacf(na.omit(allxts[,i])^2,lag.max = 100,xlab = colnames(allxts)[i], ylab = 'PACF', main=' ')
+  pacf(na.omit(allxts[,i]),lag.max = 100,xlab = colnames(allxts)[i], ylab = 'PACF', main=' ')
 }
 
 ##############GARCH model estimation, Backtesting the risk model and Forecasting#############
@@ -43,57 +44,60 @@ REIT = as.data.frame(allts[,"REIT"])
 row.names(REIT)=as.yearqtr(time(allts))
 # ARIMA 
 # ncreif
-model.arima = auto.arima(xts.ncreif , max.order = c(2, 0 ,2) , stationary = TRUE , trace = T , ic = 'bic')
+model.arima = auto.arima(xts.ncreif, stationary = TRUE , trace = T , ic = 'aic')
 
 checkresiduals(model.arima)
 # REIT
-model.arima = auto.arima(REIT, stationary = TRUE , trace = T , ic = 'aicc')
+model.arima2 = auto.arima(REIT, stationary = TRUE , trace = T , ic = 'aic')
+checkresiduals(model.arima2)
+
 # box test
-Box.test(model.arima$residuals^2,lag=12, type="Ljung-Box")
+Box.test(model.arima$residuals^2,lag = 24,type="Ljung-Box")
+Box.test(model.arima2$residuals^2, lag = 100, type="Ljung-Box")
+#colnames(allts)
 for(i in 1:ncol(allts)){
   y = allts[,i]
-  res=Box.test((y - mean(y))^2, lag=12, type="Ljung-Box")
+  res=Box.test((y - mean(y))^2, lag = 12,type="Ljung-Box")
   print(res$p.value)
 }
 
 # GARCH model
 library("rugarch")
-spec = ugarchspec(variance.model = list(model="sGARCH",
-                                        garchOrder=c(1,1)),
-                  #mean.model = list(armaOrder=c(0,0)),
+spec = ugarchspec(variance.model = list(model="sGARCH",garchOrder=c(1,1)),
+                  mean.model = list(armaOrder=c(1,1)),
                   distribution.model = "std")
 # Nelson's egarch model
-egarch.spec = ugarchspec(variance.model=list(model="eGARCH",
-                                               garchOrder=c(1,1)),
-                           #mean.model=list(armaOrder=c(0,0), include.mean=TRUE),  
-                           distribution.model="jsu", fixed.pars=list())
+egarch.spec = ugarchspec(variance.model=list(model="eGARCH",garchOrder=c(1,1)),
+                         mean.model=list(armaOrder=c(0,0)),  
+                         distribution.model="std")
 garch.fit1 = ugarchfit(egarch.spec, xts.ncreif)
+# simulation
+set.seed(123)
+tseq = seq(as.Date("1987/1/1"), as.Date("2020/6/1"), "weeks")
+sim = ugarchsim(garch.fit1,n.sim=length(tseq), n.start=0, m.sim=1, startMethod="sample")
+simseries = xts(sim@simulation$seriesSim, order.by = tseq)
+auto.arima(simseries , trace = T , ic = 'bic')
+
 
 plot(garch.fit1, which="all")
 
 # backtesting model
-ctrl = list(tol = 1e-7, delta = 1e-9)
-garchroll1 <- ugarchroll(egarch.spec, xts.ncreif, n.start = 30, refit.every = 4, refit.window = "moving", VaR.alpha = 0.01 ,solver.control = ctrl, fit.control = list(scale = 1))
-report(garchroll1, type = "VaR")
+garchroll1 <- ugarchroll(egarch.spec, data=simseries, n.start = 1000, refit.every = 100, refit.window = "moving",VaR.alpha = 0.01,solver="hybrid", fit.control = list(scale = 1))
+report(garchroll1, type = "VaR",VaR.alpha = 0.01, conf.level = 0.99)
 
 plot(garchroll1, which="all")
-
+plot(garchroll1, which=4)
 # Forecasting Risk and VaR
 garchfcst <- ugarchforecast(garch.fit1, n.ahead = 12)
 garchfcst
 plot(garchfcst,which=1)
 plot(garchfcst,which=3)
 
-garch.fit2=ugarchfit(spec,data=ncreif, solver="hybrid", out.sample=5)
+garch.fit2=ugarchfit(egarch.spec,data=xts.ncreif, solver="hybrid", out.sample=5)
 garchfcst2<-ugarchforecast(garch.fit2, data = NULL, n.ahead = 10, n.roll = 5, external.forecasts = list(mregfor = NULL, vregfor = NULL))
 garchfcst2
 plot(garchfcst2,which=2)
 plot(garchfcst2,which=4)
-
-
-
-
-
 
 
 
